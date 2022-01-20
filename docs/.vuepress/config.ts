27a87972405bc3706f013d73a40b1d2e3e246bf5
唐皓晨 <2290774104@qq.com>
2022-01-20 17:13:16 +0800

    chore: 引入 vdoing 主题

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,18 +1,58 @@
-import { defineConfig } from 'vuepress/config'
-export default defineConfig({
-  title: 'tang’s blog',
+import { defineConfig4CustomTheme } from 'vuepress/config';
+import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types';
+export default defineConfig4CustomTheme<VdoingThemeConfig>({
+  theme: 'vdoing',
+  title: "tang's blog",
   description: '唐皓晨的个人博客',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.jpg' }]
-  ],
+  head: [['link', { rel: 'icon', href: '/favicon.jpg' }]],
   markdown: {
     lineNumbers: true
   },
   themeConfig: {
     nav: [
-      { text: 'guide', link: '/guide/' },
+      { text: '首页', link: '/' },
+      {
+        text: '前端',
+        items: [
+          {
+            text: '学习笔记',
+            items: [
+              { text: 'VuePress', link: '/pages/b42ac3/' },
+              { text: 'vuepress-theme-vdoing', link: '/pages/28a711/' }
+            ]
+          }
+        ]
+      },
+      {
+        text: '索引',
+        link: '/archives/',
+        items: [
+          { text: '分类', link: '/categories/' },
+          { text: '标签', link: '/tags/' },
+          { text: '归档', link: '/archives/' }
+        ]
+      }
     ],
-    sidebar: 'auto',
-    sidebarDepth: 2
+    sidebar: 'structuring',
+    sidebarDepth: 2,
+    // repo: 'tang-haochen/personal-blog', // 导航栏右侧生成Github链接
+    searchMaxSuggestions: 10,
+    lastUpdated: '上次更新',
+    // docsDir: 'docs',
+    // editLinks: true,
+    // editLinkText: '编辑',
+    author: {
+      name: 'tanghc'
+    },
+    blogger: {
+      avatar: '',
+      name: 'tanghc',
+      slogan: '前端学习者'
+    },
+    // 页脚信息
+    footer: {
+      createYear: 2022, // 博客创建年份
+      copyrightInfo: ''
+    }
   }
-})
+});
